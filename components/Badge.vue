4bf0d4569729b23837036b38076f1626041dd024
Brandel-T <tsagueubrandel@gmail.com>
2023-11-20 03:54:42 +0100

    [EDIT] badge component: primary and secondary theme

--- components/Badge.vue
+++ components/Badge.vue
@@ -2,11 +2,21 @@
   <div :class="['e-badge', badgeBackground]">
     <img
       v-if="useIcon(iconName!)"
-      :class="['e-badge-icon', `!bg-${color}`]"
+      :class="[
+        'e-badge-icon',
+        { 'bg-accent': theme !== 'secondary' },
+        { 'bg-secondary': theme === 'secondary' },
+      ]"
       :src="String(useIcon(iconName!))"
       :alt="`svg icon: ${iconName}`"
     />
-    <div :class="['e-badge-content', `text-${color}`]">
+    <div
+      :class="[
+        'e-badge-content',
+        { 'text-accent': theme !== 'secondary' },
+        { 'text-secondary': theme === 'secondary' },
+      ]"
+    >
       {{ value }}
     </div>
   </div>
@@ -21,7 +31,6 @@ export default defineNuxtComponent({
   props: {
     value: { type: String, required: false },
     iconName: { type: String, required: false },
-    iconColor: { type: String, required: false },
     theme: { type: String as PropType<TBadgeTheme>, default: "primary" },
   },
   setup(props: any) {
@@ -29,10 +38,7 @@ export default defineNuxtComponent({
     const badgeBackground = computed(() =>
       props.theme === "secondary" ? "bg-tertiary" : "bg-accent-soft",
     );
-    const color = computed(() =>
-      props.theme === "secondary" ? "secondary" : "accent",
-    );
-    return { icon, badgeBackground, color };
+    return { icon, badgeBackground };
   },
 });
 </script>
