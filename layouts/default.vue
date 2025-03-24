e2484092d365e606ba2702e41d29cab858ed14b3
Brandel-T <tsagueubrandel@gmail.com>
2025-03-24 18:44:16 +0100

    display maintenance page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
-    <NavBar class="sticky top-0" />
-    <main>
+    <NavBar v-if="false" class="sticky top-0" />
+
+    <div
+      class="h-screen text-center align-middle flex flex-col gap-8 justify-center items-center"
+    >
+      <div class="text-xl">The site is currently undergoing maintenance.</div>
+      <Badge icon-name="maintenance" theme="primary" />
+    </div>
+
+    <main v-if="false">
       <slot />
     </main>
-    <MyFooter />
+    <MyFooter v-if="false" />
   </div>
 </template>
+
+<style scoped lang="scss">
+:deep(.e-badge) {
+  width: 14rem;
+  height: 14rem;
+  background: $text-color-2;
+
+  .e-badge-icon {
+    padding: 2rem;
+    height: 80%;
+    width: 80%;
+    background: none;
+    border-radius: 0;
+    margin-inline: auto;
+  }
+}
+</style>
